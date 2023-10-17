d324538deda1b1af6bf8597901858be518df3158
warrenronsiek <warrenronsiek@gmail.com>
2023-10-17 10:54:43 -0400

    removed rand partition

--- src/main/scala/com/amicusearch/etl/utils/ParquetWriter.scala
+++ src/main/scala/com/amicusearch/etl/utils/ParquetWriter.scala
@@ -1,26 +1,15 @@
 package com.amicusearch.etl.utils
 
 import com.typesafe.scalalogging.LazyLogging
-import org.apache.hadoop.conf.Configuration
-import org.apache.spark.sql.expressions.Window
-import org.apache.spark.sql.functions._
-import org.apache.spark.sql.types.{LongType, StringType}
+import org.apache.spark.sql.streaming.OutputMode
 import org.apache.spark.sql.{Dataset, SQLContext, SaveMode, SparkSession}
 
-class ParquetWriter(writePath: String, partitionCols: List[String], saveMode: SaveMode = SaveMode.Append)
-                     (implicit spark: SparkSession, sqlContext: SQLContext) extends LazyLogging {
+class ParquetWriter(writePath: String, partitionCols: List[String])
+                   (implicit spark: SparkSession, sqlContext: SQLContext) extends LazyLogging {
   val write: Dataset[_] => Unit = ds => {
-
-    val window = Window.partitionBy(partitionCols.head).orderBy(col(partitionCols.head).desc)
-    val newPartitions = partitionCols :+ "rand_partition"
-
-    // that we repartition by rand_partition means that we ensure that each file written out contains no more than
-    // 5000000 records. This is important because otherwise repartitioning by a large partition will cause OOM
-    ds.withColumn("rand_partition", (row_number.over(window) / 500000).cast(LongType))
-      .repartition(newPartitions.map(s => col(s)):_*)
-      .drop("rand_partition")
-      .writeStream
+    ds.writeStream
       .partitionBy(partitionCols: _*)
+      .outputMode(OutputMode.Append())
       .format("parquet")
       .option("path", writePath)
       .start()
@@ -28,8 +17,8 @@ class ParquetWriter(writePath: String, partitionCols: List[String], saveMode: Sa
 }
 
 object ParquetWriter {
-  def apply(writePath: String, partitionCols: List[String], saveMode: SaveMode = SaveMode.Overwrite)
+  def apply(writePath: String, partitionCols: List[String])
            (implicit spark: SparkSession, sqlContext: SQLContext): ParquetWriter =
-    new ParquetWriter(writePath, partitionCols, saveMode)
+    new ParquetWriter(writePath, partitionCols)
 
 }
