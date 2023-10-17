3c987a9b39c65f2f78dc71bd61de5ff1cd0573e8
warrenronsiek <warrenronsiek@gmail.com>
2023-10-16 23:45:46 -0400

    changed SparkConf to include checkpoint

--- src/main/scala/com/amicusearch/etl/Main.scala
+++ src/main/scala/com/amicusearch/etl/Main.scala
@@ -7,16 +7,16 @@ import org.apache.spark.sql.{SQLContext, SparkSession}
 
 object Main extends LazyLogging {
   def run(params: AppParams): Unit = {
-    implicit val spark: SparkSession = SparkSession.builder
-      .config(SparkConf.conf).getOrCreate()
-    implicit val sc: SparkContext = spark.sparkContext
-    implicit val sql: SQLContext = spark.sqlContext
     val conf: Config = ConfigFactory.load(params.env match {
       case AppParams.Environment.prod => "prod.conf"
       case AppParams.Environment.dev => "dev.conf"
       case AppParams.Environment.local => "local.conf"
       case AppParams.Environment.cci => "cci.conf"
     })
+    implicit val spark: SparkSession = SparkSession.builder
+      .config(SparkConf.sparkConf(conf.getString("results.checkpoint"))).getOrCreate()
+    implicit val sc: SparkContext = spark.sparkContext
+    implicit val sql: SQLContext = spark.sqlContext
 
     params.mode match {
       case AppParams.Mode.courtListener => RunCourtlistener(params, conf)
--- src/main/scala/com/amicusearch/etl/SparkConf.scala
+++ src/main/scala/com/amicusearch/etl/SparkConf.scala
@@ -1,10 +1,13 @@
 package com.amicusearch.etl
 
+import com.typesafe.config.Config
 import org.apache.spark.SparkConf
 
 object SparkConf {
-  val conf = new SparkConf()
+
+  def sparkConf(checkpointLocation: String): SparkConf = new SparkConf()
     .set("spark.sql.files.maxRecordsPerFile", "20000000")
     .set("spark.sql.parquet.binaryAsString", "true")
     .set("spark.sql.session.timeZone", "UTC")
+    .set("spark.sql.streaming.checkpointLocation", checkpointLocation)
 }
