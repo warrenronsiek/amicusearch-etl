d5da13419f1ee4e1a7d61c325326bebf77df5a43
warrenronsiek <warrenronsiek@gmail.com>
2023-10-21 22:23:22 -0400

    added retry to Summarizer

--- src/main/scala/com/amicusearch/etl/utils/Summarizer.scala
+++ src/main/scala/com/amicusearch/etl/utils/Summarizer.scala
@@ -2,6 +2,7 @@ package com.amicusearch.etl.utils
 
 import com.amicusearch.etl.AppParams
 import com.typesafe.config.{Config, ConfigFactory}
+import com.typesafe.scalalogging.LazyLogging
 
 import scala.concurrent.duration._
 import util.retry.blocking.{Failure, Retry, RetryStrategy, Success}
@@ -9,25 +10,33 @@ import upickle.default._
 import upickle.default.{macroRW, ReadWriter => RW}
 import util.retry.blocking.RetryStrategy.RetryStrategyProducer
 
-class Summarizer(env: AppParams.Environment.Value, summaryUrl: String) extends java.io.Serializable {
+class Summarizer(env: AppParams.Environment.Value, summaryUrl: String) extends java.io.Serializable with LazyLogging {
 
   private case class Response(summary: String) extends java.io.Serializable
+
   private object Response {
     implicit val rw: RW[Response] = macroRW
   }
 
   private case class Request(text: String) extends java.io.Serializable
+
   private object Request {
     implicit val rw: RW[Request] = macroRW
   }
 
-  implicit val retryStrategy: RetryStrategyProducer = RetryStrategy.fibonacciBackOff(3.seconds, maxAttempts = 30)
+  implicit val retryStrategy: RetryStrategyProducer = RetryStrategy.fibonacciBackOff(0.5.seconds, maxAttempts = 30)
 
   val summarize: String => String = (text: String) => {
     env match {
       case AppParams.Environment.prod | AppParams.Environment.dev =>
-        val req = requests.post(summaryUrl, headers = Map("Content-Type" -> "application/json"), data = write(Request(text)))
-        read[Response](req.text).summary
+        Retry(requests.post(summaryUrl, headers = Map("Content-Type" -> "application/json"),
+          data = write(Request(text)))
+        ) match {
+          case Success(r) => read[Response](r.text).summary
+          case Failure(e) =>
+            logger.error(s"Failed to summarize with error: ${e.getMessage}")
+            throw e
+        }
       case _ => read[Response]("""{"summary":"stub"}""").summary
     }
   }
